7f636342f0effa380b10b9c9945de19f07bf30cb
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-22 16:41:06 +0000

    adding corse - connecting with frontend

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { linksRouter } from "./routes/links.routes.js";
+import cors from "cors"
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use("/api/links", linksRouter);
