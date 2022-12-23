ab058c25fc44e37cd6832ddcbf1af04660e9d61c
emilykpritchard <emilykpritchard@me.com>
2022-12-23 15:01:46 +0000

    New routes for new search field

--- models/links.model.js
+++ models/links.model.js
@@ -1,9 +1,18 @@
 import { pool } from "../db/index.js";
 
+export async function getLinksBySearchTerm(searchTerm) {
+  const sqlQuery =
+    "SELECT * FROM links INNER JOIN subjects ON links.subject = subjects.subject_id WHERE links.description iLIKE ($1) OR subjects.subject iLIKE ($1)";
+  const parameterValue = [`%${searchTerm}%`];
+  const result = await pool.query(sqlQuery, parameterValue);
+  const links = result.rows;
+  return links;
+}
+
 export async function getLinksByWeek(week) {
   const sqlQuery =
     "SELECT * FROM links INNER JOIN subjects ON links.subject = subjects.subject_id WHERE week = ($1) ORDER BY likes DESC;";
-    const parameterValues = [week]
+  const parameterValues = [week];
   const result = await pool.query(sqlQuery, parameterValues);
   const links = result.rows;
   return links;
@@ -12,7 +21,7 @@ export async function getLinksByWeek(week) {
 export async function getLinksBySubject(subject) {
   const sqlQuery =
     "SELECT * FROM links INNER JOIN subjects ON links.subject = subjects.subject_id WHERE subjects.subject_id = ($1) ORDER BY likes DESC;";
-    const parameterValues = [subject]
+  const parameterValues = [subject];
   const result = await pool.query(sqlQuery, parameterValues);
   const links = result.rows;
   return links;
@@ -21,17 +30,23 @@ export async function getLinksBySubject(subject) {
 export async function createLink(newLink) {
   const sqlQuery =
     "INSERT INTO links (link, title, description, week, subject) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
-  const parameterValues = [newLink.link, newLink.title, newLink.description, newLink.week, newLink.subject];
+  const parameterValues = [
+    newLink.link,
+    newLink.title,
+    newLink.description,
+    newLink.week,
+    newLink.subject,
+  ];
   const result = await pool.query(sqlQuery, parameterValues);
   const created = result.rows[0];
   return created;
 }
 
 export async function countLikes(id, linksCard) {
-  const sqlQuery = "UPDATE links SET likes = ($1) WHERE link_id = $2 RETURNING * ;";
+  const sqlQuery =
+    "UPDATE links SET likes = ($1) WHERE link_id = $2 RETURNING * ;";
   const parameterValues = [linksCard.likes, id];
   const result = await pool.query(sqlQuery, parameterValues);
-  const updated = result.rows[0]
+  const updated = result.rows[0];
   return updated;
 }
-
--- routes/links.routes.js
+++ routes/links.routes.js
@@ -3,6 +3,15 @@ import * as linksModel from "../models/links.model.js";
 
 export const linksRouter = express.Router();
 
+linksRouter.get("/", async function (req, res) {
+  const links = await linksModel.getLinksBySearchTerm(req.query.searchTerm);
+
+  res.status(200).json({
+    success: true,
+    payload: links,
+  });
+});
+
 linksRouter.get("/:week", async function (req, res) {
   const links = await linksModel.getLinksByWeek(req.params.week);
 
@@ -11,7 +20,6 @@ linksRouter.get("/:week", async function (req, res) {
     payload: links,
   });
 });
-
 
 linksRouter.get("/", async function (req, res) {
   const links = await linksModel.getLinksBySubject(req.query.subject);
@@ -24,7 +32,11 @@ linksRouter.get("/", async function (req, res) {
 
 linksRouter.post("/", async function (req, res) {
   const somethingIsMissing =
-  req.body.link === undefined || req.body.title === undefined || req.body.description === undefined || req.body.week === undefined || req.body.subject === undefined;
+    req.body.link === undefined ||
+    req.body.title === undefined ||
+    req.body.description === undefined ||
+    req.body.week === undefined ||
+    req.body.subject === undefined;
 
   if (somethingIsMissing) {
     res.status(400).json({
@@ -42,11 +54,10 @@ linksRouter.post("/", async function (req, res) {
   });
 });
 
-linksRouter.patch("/:id", async function (req, res){
-  const updatedLikes = await linksModel.countLikes(req.params.id,req.body);
-  console.log(updatedLikes)
-    res.status(200).json({
+linksRouter.patch("/:id", async function (req, res) {
+  const updatedLikes = await linksModel.countLikes(req.params.id, req.body);
+  res.status(200).json({
     success: true,
     payload: updatedLikes,
-  })
+  });
 });
